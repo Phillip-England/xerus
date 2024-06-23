39b0939839803c4debd9c0b3b6553da8cd2e873e
Phillip England <phillip.mark.england@gmail.com>
2024-06-23 18:06:56 -0500

    working on bun create messaging

---
+++ scripts/postcreate.js
@@ -0,0 +1,15 @@
+console.log(`
+    Come hang out in bun's Discord: https://bun.sh/discord
+    
+    -----
+    
+    A local git repository was created for you and dependencies were installed automatically.
+    
+    Success! phillip-england/xerus loaded into <your-app>
+    
+    # To get started, run:
+    
+      cd <your-app>
+      bun install
+      bun run dev
+    `);
