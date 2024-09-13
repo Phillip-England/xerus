d49a671f1408125f82d967e1659baf5e881b2c55
Phillip-England <phillip.mark.england@gmail.com>
2024-09-13 15:28:45 -0500

    created .get .post ect methods on Xerus

--- index.jsx
+++ index.jsx
@@ -1,43 +1,39 @@
-import { Xerus } from "./Xerus"
-import { logger, timeout } from "./XerusMiddleware"
+import { Xerus } from "./Xerus";
+import { logger, timeout } from "./XerusMiddleware";
 
-let app = new Xerus()
+let app = new Xerus();
 
-app.setTimeoutDuration(5000)
-app.use('*', timeout, logger)
+app.setTimeoutDuration(5000);
+app.use("*", timeout, logger);
 
 let Layout = (props) => {
-    return (
-        <html>
-            <head>
-                <meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="stylesheet" href="/static/css/output.css" />
-				<script src='/static/js/index.js'></script>
-                <title>{props.title}</title>
-            </head>
-            <body>
-                {props.children}
-            </body>
-        </html>
-    )
-}
- 
-app.at('GET /', async (c) => {
-    c.jsx(
-        <Layout title="test">
-            <p>yoooooo</p>
-        </Layout>
-    )
-})
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="/static/css/output.css" />
+        <script src="/static/js/index.js"></script>
+        <title>{props.title}</title>
+      </head>
+      <body>{props.children}</body>
+    </html>
+  );
+};
 
-app.at('POST /', async (c) => {
-    let data = await c.form()
-    let photo = data.get('photo')
-    await Bun.write('photo.jpg', photo);
-    c.redirect("/")
-})
+app.get("/", async (c) => {
+  c.jsx(
+    <Layout title="test">
+      <p>yoooooo</p>
+    </Layout>,
+  );
+});
 
-await app.run(8080)
+app.post("/", async (c) => {
+  let data = await c.form();
+  let photo = data.get("photo");
+  await Bun.write("photo.jpg", photo);
+  c.redirect("/");
+});
 
-
+await app.run(8080);
