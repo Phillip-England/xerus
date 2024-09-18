cf18281f4d7656ebab3744db6aa312ae54a5112c
Phillip-England <phillip.mark.england@gmail.com>
2024-09-17 19:27:52 -0500

    added test for network timeouts

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,26 +1,30 @@
 import { expect, test } from "bun:test";
 
-import { logger, Xerus, XerusContext } from ".";
+import { logger, timeout, Xerus, XerusContext } from ".";
 import { sleep } from "bun";
 
-function server(routeSetup: (app: Xerus) => void, testFn: Function) {
+test("hello world", async () => {
   const app: Xerus = new Xerus();
-  routeSetup(app);
-  app.run(8080);
-  testFn();
-}
+  app.get("/", async (c: XerusContext) => {
+    c.text("hello, world");
+  });
+  await app.run(8080);
+  let res = await fetch("localhost:8080/");
+  let text = await res.text();
+  expect(text).toBe("hello, world");
+});
 
-test("hello", async () => {
-  server(
-    (app) => {
-      app.get("/", async (c: XerusContext) => {
-        c.text("hello, world");
-      });
-    },
-    async () => {
-      let res = await fetch("localhost:8080/");
-      let text = await res.text();
-      expect(text).toBe("hello, world");
-    },
-  );
+test("timeout", async () => {
+  const app: Xerus = new Xerus();
+  app.setTimeoutDuration(1000);
+  app.use("*", timeout);
+  app.get("/slow", async (c: XerusContext) => {
+    await sleep(3000);
+    c.text("This will never be reached due to timeout");
+  });
+  await app.run(8080);
+  const res = await fetch("http://localhost:8080/slow");
+  const text = await res.text();
+  expect(res.status).toBe(504);
+  expect(text).toBe(JSON.stringify({ error: "request timed out" }));
 });
