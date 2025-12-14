cdfd29e7f6d8375c94a3a7228a618b0d894fc8a1
Phillip England <phillip.mark.england@gmail.com>
2025-12-14 12:24:19 -0600

    testing to see if circular import issue is resolved

--- myapp/index.ts
+++
@@ -1,9 +0,0 @@
-import { FileRouter } from "xerus";
-import path from "path";
-
-let router = await FileRouter.new({
-  "src": path.join(process.cwd(), "app"),
-  "port": 8080,
-});
-
-await router.listen()
